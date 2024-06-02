f79c9ae4a41b51ea7f76080a32b289b002caa863
Hugo mendonca <hugomendonca9@gmail.com>
2024-06-02 15:46:58 -0300

    Add configurações do eslint e prettier para o astro

--- src/components/Teste.tsx
+++ src/components/Teste.tsx
@@ -1,7 +1,7 @@
 export default function TesteComponent() {
-    return (
-        <div>
-            <h1>Teste e component</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Teste e component</h1>
+    </div>
+  );
 }
